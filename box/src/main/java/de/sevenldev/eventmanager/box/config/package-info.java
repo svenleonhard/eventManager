c188fb503c1a73be8d8541e4290070421f154737
svenleonhard <40168082+svenleonhard@users.noreply.github.com>
2019-07-07 15:46:17 +0200

    Initial application generated by JHipster-6.1.2

---
+++ box/src/main/java/de/sevenldev/eventmanager/box/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package de.sevenldev.eventmanager.box.config;
---
+++ box/src/main/java/de/sevenldev/eventmanager/box/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service layer beans.
+ */
+package de.sevenldev.eventmanager.box.service;
